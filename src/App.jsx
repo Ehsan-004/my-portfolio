966454e7ff07bf1f09020a720b51c59d544ce8b3
Ehsan-004 <ehsankholusi@gmail.com>
2026-06-01 13:03:46 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Building it.</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
